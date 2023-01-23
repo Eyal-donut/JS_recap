1085522de8b06e96836b03e1b58b1c6a01a11c19
Eyal-donut <LilaEyal@gmail.com>
2023-01-23 12:37:23 +0100

    commit_lunch_break

--- text_functions.js
+++ text_functions.js
@@ -1,13 +1,9 @@
 //Main Text Functions file
 
 
-const testText1 =
-    "Once upon a time there was a dear little girl who was loved by everyone who looked at her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave her a little cap of red velvet, which suited her so well that she would never wear anything else; so she was always called ‘Little Red-Cap.’";
-
 const testText1 = "Once upon a time there was a dear little girl who was loved by everyone who looked at her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave her a little cap of red velvet, which suited her so well that she would never wear anything else; so she was always called ‘Little Red-Cap.’";
 
-
-const testText2 = "Once upon a time. there was a dear little girl.";
+const testText2 = "Once upon a time there was a dear little girl.";
 
 const wordCount = (text) => {
 
@@ -19,6 +15,7 @@ const wordCount = (text) => {
     }
     return wordCounter;
 };
+
 console.log(wordCount(testText2));
 
 function averageWordLength(text) {
@@ -37,14 +34,7 @@ function averageWordLength(text) {
 
 console.log(averageWordLength(testText2));
 
-  let wordCounter = 1;
-  for (let i = 0; i < text.length; ++i) {
-    if (text[i] === " ") {
-      ++wordCounter;
-    }
-  }
-  return wordCounter;
-};
+ 
 
 console.log(wordCount(testText2));
 
