9b1ddc3e64ea083722c070a92c6592a221514de6
Eyal-donut <LilaEyal@gmail.com>
2023-01-23 11:56:28 +0100

    word_counter_done

--- text_functions.js
+++ text_functions.js
@@ -4,8 +4,17 @@
 
 const testText1 = "Once upon a time there was a dear little girl who was loved by everyone who looked at her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave her a little cap of red velvet, which suited her so well that she would never wear anything else; so she was always called ‘Little Red-Cap.’"
 
-const testText2 = "Once upon a time there was a dear little girl"
+const testText2 = "Once upon a time. there was a dear little girl."
 
-
+const wordCount = (text) => {
+    let wordCounter = 1;
+    for (let i = 0; i < text.length; ++i){
+        if (text[i] === " ") {
+            ++wordCounter
+        }
+    }
+    return wordCounter
+}
+console.log(wordCount(testText2))
 
 
