3a874a1e5571fd27afe5869ca384513279eb530a
Digvorzhak <shon.h777@gmail.com>
2023-01-23 13:19:43 +0200

    added a feature manually

--- text_functions.js
+++ text_functions.js
@@ -2,19 +2,28 @@
 
 //Main Text Functions file
 
-const testText1 = "Once upon a time there was a dear little girl who was loved by everyone who looked at her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave her a little cap of red velvet, which suited her so well that she would never wear anything else; so she was always called ‘Little Red-Cap.’"
+const testText1 = "Once upon a time there was a dear little girl who was loved by everyone who looked at her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave her a little cap of red velvet, which suited her so well that she would never wear anything else; so she was always called ‘Little Red-Cap.’";
 
-const testText2 = "Once upon a time. there was a dear little girl."
+const testText2 = "Once upon a time. there was a dear little girl.";
 
 const wordCount = (text) => {
-    let wordCounter = 1;
-    for (let i = 0; i < text.length; ++i){
-        if (text[i] === " ") {
-            ++wordCounter
-        }
+  let wordCounter = 1;
+  for (let i = 0; i < text.length; ++i) {
+    if (text[i] === " ") {
+      ++wordCounter;
     }
-    return wordCounter
-}
-console.log(wordCount(testText2))
+  }
+  return wordCounter;
+};
+console.log(wordCount(testText2));
 
+const sentenceCount = (text) => {
+  let modifiedText = text;
+  modifiedText = modifiedText.replace(/,/g, ".");
+  modifiedText = modifiedText.replace(/;/g, ".");
+  console.log(modifiedText);
+  const sentences = modifiedText.split(".");
+  return sentences.length - 1;
+};
 
+console.log(sentenceCount(testText1));
